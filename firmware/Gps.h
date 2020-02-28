5aab1c54d8acc19999d9bdb69ec1b353d2ab364a
Andrew Schechter <schechter@g.ucla.edu>
2020-02-28 00:36:41 -0800

    initial commit

---
+++ firmware/Gps.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "RocketModule.h"
+#include <TinyGPS++.h>
+
+#define RX_GPS 9
+#define TX_GPS 10
+#define GPS_BAUD 9600
+
+namespace Gps{
+    float &lat = Rocket::data.Gps_lat;
+    float &lng = Rocket::data.Gps_lng;
+    
+    UART gpsSerial(digitalPinToPinName(RX_GPS), digitalPinToPinName(TX_GPS), NC, NC);
+
+    class Handler: public Rocket::RocketModule {
+    public:
+
+        TinyGPSPlus gps;
+
+        virtual bool warmup() {
+            bool setupRight = gpsSerial.begin(GPS_BAUD);
+            if(!setupRight) {
+                return false;
+            }
+
+            return setupRight;
+        }
+        virtual void refresh() {
+            
+            if(!gps.available() || !gps.location.isUpdated()) {
+                return;
+            }
+
+            lat = gps.location.lat();
+            lng = gps.location.lng();   
+        }
+    }
+}:
