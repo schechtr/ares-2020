49d64615300a8ffb40f16d3de3437d9fde52a23b
Hamlin Liu <hamlin.liu@gmail.com>
2020-03-06 14:59:29 -0800

    updating port select too cleaner code

--- ground/portSelect.py
+++ ground/portSelect.py
@@ -1,38 +1,11 @@
-import serialFunctions
-import glob
-
-# def portSelect():
-#     '''
-#     finds viable Serial ports for you
-#     returns some
-#     '''
-
 import serial.tools.list_ports
-ports = serial.tools.list_ports.comports()
-
-USB_DIRECTORY = '/dev/cu.usb*'
-
-# try:
-#     ports = glob.glob(USB_DIRECTORY)
-#     if len(ports) > 1:
-#         print('More than one USB device detected. Please disconnect all other USB devices')
-
-#     serial_ports = ports[0]
-
-#     print(ports)
-# except:
-#     print('Could not find a usb device')
-
-
-# print("Found the following ports:")
-
-# for port, desc, hwid in sorted(ports):
-#     print("{}: {} [{}]".format(port, desc, hwid))
 
 def port_select():
-
-    print("hamlin has a small pp")
-
+    '''
+    finds viable Serial ports for you
+    returns the good port
+    '''
+    ports = serial.tools.list_ports.comports()
     serial_ports = list()
     for port in sorted(ports):
         if port.description != 'n/a':
