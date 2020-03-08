403a0a67a9b90c76b45d5718d8584f05c62060cb
Andrew Schechter <schechter@g.ucla.edu>
2020-03-08 16:05:37 -0700

    initial commit

---
+++ ground/ground.py
@@ -0,0 +1,46 @@
+
+import serial
+import serialparser
+from portSelect import serial_ports
+
+
+def main():
+    
+    print('Scanning ports...')    
+    # select the port
+    ports = serial_ports()
+    print(ports)
+
+    indexNum = int(input("Select index: "))
+    port = ports[indexNum]
+    
+    receive = serial.Serial(port, baudrate=57600, timeout=5)
+    print("Connection successful, listening on", port)
+    
+    # class instances
+    data = serialparser.RocketData()
+    parser = serialparser.SerialParser(data)
+
+    while True:
+        stream = receive.read(2400)
+        
+        parser.findPackage(stream)
+
+        if len(parser.data_queue) > 25:
+            break
+
+        
+    print("report:")
+    print(parser.data_queue)
+    print("total packages received: {}".format(parser.numpackages))
+    print("total packages lost: {}".format(parser.loss))
+    print("percent lost: {}".format(100.0 * parser.loss / parser.numpackages))
+
+    
+    receive.close()
+
+    
+
+if __name__ == "__main__":
+    main()
+
