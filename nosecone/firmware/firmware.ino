7e54427fced038c4d2595d14a376321351c6b541
Andrew Schechter <schechter@g.ucla.edu>
2020-03-09 14:47:10 -0700

    benchmark hz

--- nosecone/firmware/firmware.ino
+++ nosecone/firmware/firmware.ino
@@ -1,7 +1,6 @@
 #include "RocketModule.h"
 #include "rocket.h"
 
-bool isOn = true;
 
 void preWarmup();
 void warmup();
@@ -15,36 +14,37 @@ bool byteFlag(uint8_t b, int idx) {
 }
 
 void setup() {
+
 	Serial.begin(115200);
+
 	uint32_t serialAwaitStart = millis();
 	while(millis() - serialAwaitStart < 3000 && !Serial);
+
 	Serial.println("Congratulations on your code compiling");
 	preWarmup();
 	warmup();
+
 	Serial.println("Flushing remaining bytes");
 	uint32_t flushStart = millis();
 	while(millis() - flushStart < 2000 && Serial.read() > 0);
-	pinMode(LED_BUILTIN, OUTPUT);
+    
 	Serial.println("Setup function complete, beginning telemetry");
 }
 
-
-uint32_t totalRefresh = 0;
-bool highLoop = true;
+__uint32_t refreshCount = 0;
+__uint32_t totalRefreshTime = 0;
 void loop() {
-	if(isOn) {
-		if(highLoop) {
-			digitalWrite(LED_BUILTIN, HIGH);
-		}
-		else {
-			digitalWrite(LED_BUILTIN, LOW);
-		}
-		refresh();
-		highLoop = !highLoop;
-	}
-	else {
-		digitalWrite(LED_BUILTIN, LOW);
-	}
+
+    uint32_t start = millis();
+    refresh();
+    uint32_t refreshTime = millis() - start;
+    totalRefreshTime += refreshTime;
+    refreshCount++;
+
+    if(refreshCount > 1000) {
+        __int32_t avg = totalRefreshTime / refreshCount;
+        Serial.println(avg);
+    }
 }
 
 
