893ed7045f8a90fc7c35dbd7cf3d5e80de730002
Andrew Schechter <schechter@g.ucla.edu>
2020-03-12 15:58:27 -0700

    cleaned up

--- bodytube/firmware/firmware.ino
+++ bodytube/firmware/firmware.ino
@@ -1,8 +1,6 @@
 #include "RocketModule.h"
 #include "rocket.h"
 
-bool isOn = true;
-
 void preWarmup();
 void warmup();
 void refresh();
@@ -16,35 +14,27 @@ bool byteFlag(uint8_t b, int idx) {
 
 void setup() {
 	Serial.begin(115200);
+
 	uint32_t serialAwaitStart = millis();
 	while(millis() - serialAwaitStart < 3000 && !Serial);
 	Serial.println("Congratulations on your code compiling");
+
 	preWarmup();
 	warmup();
+
 	Serial.println("Flushing remaining bytes");
 	uint32_t flushStart = millis();
 	while(millis() - flushStart < 2000 && Serial.read() > 0);
 	pinMode(LED_BUILTIN, OUTPUT);
+
 	Serial.println("Setup function complete, beginning telemetry");
 }
 
-
-uint32_t totalRefresh = 0;
-bool highLoop = true;
+// add refresh time benchmarks here
 void loop() {
-	if(isOn) {
-		if(highLoop) {
-			digitalWrite(LED_BUILTIN, HIGH);
-		}
-		else {
-			digitalWrite(LED_BUILTIN, LOW);
-		}
-		refresh();
-		highLoop = !highLoop;
-	}
-	else {
-		digitalWrite(LED_BUILTIN, LOW);
-	}
+
+    refresh();
+    
 }
 
 
