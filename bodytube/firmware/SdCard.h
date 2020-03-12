8d870d43b702b50f8b1cc6b2e9728b225abdd976
Andrew Schechter <schechter@g.ucla.edu>
2020-03-12 15:55:06 -0700

    updated with code that was tested on the nosecone system

--- bodytube/firmware/SdCard.h
+++ bodytube/firmware/SdCard.h
@@ -1,58 +1,45 @@
 #pragma once
+#include <SPI.h>
 #include <SD.h>
 #include "RocketModule.h"
 
-#define SD_CHIPSEL 9
+const int chipSelect = 3;
 
 
 namespace SdCard {
     class Handler : public Rocket::RocketModule {
+    
     private:
-        int bytesWritten = 0;
-        const char *fileName = "rocket_data.bin";
-        SDLib::File saveFile;
+        File dataFile;
+        char filename[13] = "Rocket_Data";
 
     public:
         virtual bool warmup() {
-            if (!SD.begin(SD_CHIPSEL)) {
-                Serial.println("SD initialization failed");
-                return false;
+
+            if (!SD.begin(chipSelect)) {
+                
+                while(1) {
+                    Serial.println("Card failed, or not present");
+                }
             }
-            Serial.print("Trying to create file: ");
-            if (saveFile = SD.open(fileName, FILE_WRITE)) {
-                Serial.println("Created file");
-                return true;
-            } else {
-                Serial.println("Could not create file.");
-                return false;
-            }
-            Serial.println("all good");
-            return true;
+        
+           return true;
         }
         virtual void refresh() {
-            // Remainder will store how many bytes overflow
-            // Assume max size is 8 bytes, data len is 3, bytes written is 7
-            int remainder = bytesWritten + Rocket::DATA_LEN - 512;  // 2
-            remainder = max(0, remainder);                          // 2
-            int k = saveFile.write(Rocket::DATA_START);  // writes [data+0, 1)
+           
+           dataFile = SD.open(filename, FILE_WRITE);
 
-            if (bytesWritten + Rocket::DATA_LEN >= 512) {  // if 7 + 3 >= 8 (true)
-                //int startMillis = millis();
-                saveFile.close();  // flush
-                saveFile = SD.open(fileName, FILE_WRITE);
-                bytesWritten = remainder;
-                if (bytesWritten > 0) {  // true
-                    k += saveFile.write(
-                        Rocket::DATA_START + Rocket::DATA_LEN - remainder,
-                        remainder);  // writes [data+2, 6)
-                }                    // 4
+           if(dataFile) {
+               dataFile.write(Rocket::DATA_START, Rocket::DATA_LEN);
+               dataFile.close();
+           }
+           else {
+               Serial.println("error opening file");
+           }
 
-            } else {
-                bytesWritten += Rocket::DATA_LEN;
-            }
         }
         virtual void shutdown() { 
-            saveFile.close(); 
+            dataFile.close(); 
         }
     }; 
-}  // namespace SdCard
+}
